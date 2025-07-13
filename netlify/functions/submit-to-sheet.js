c38892e55095e6a6314da72534715674f21f5861
arltrol <roland.arlt@gmail.com>
2025-07-13 15:13:04 +0100

    Create submit-to-sheet.js

---
+++ netlify/functions/submit-to-sheet.js
@@ -0,0 +1,32 @@
+// /netlify/functions/submit-to-sheet.js
+
+export async function handler(event) {
+  const targetUrl = "https://script.google.com/macros/s/AKfycbzjTlifICLdi4k-LmxIRz4nFOIfgvhsEQsle6Nm4hWfyZTlZEnmDnNi7GDCOnYHxwRi/exec";
+
+  try {
+    const googleResponse = await fetch(targetUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: event.body
+    });
+
+    const text = await googleResponse.text();
+
+    return {
+      statusCode: 200,
+      body: text,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Content-Type": "text/plain"
+      }
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: `Failed to forward: ${err.message}`
+    };
+  }
+}
